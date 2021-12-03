b5211866822d1e7b259e07e4732da6f40958fe4c
soyeonnnb <la28s5d@naver.com>
2021-12-04 02:12:47 +0900

    feat: review 1~5

--- bftour/reviews/forms.py
+++ bftour/reviews/forms.py
@@ -7,3 +7,11 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = models.Review
         fields = ("review", "rating")
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+
+        self.fields["rating"].widget.attrs = {
+            "max": 5,
+            "min": 1,
+        }
