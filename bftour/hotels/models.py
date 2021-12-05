9da20464b7b70aacc12ea26e3ecedbb0f102ce71
soyeonnnb <la28s5d@naver.com>
2021-12-05 14:18:41 +0900

    Update models.py

--- bftour/hotels/models.py
+++ bftour/hotels/models.py
@@ -16,40 +16,6 @@ class Post(models.Model):
 # 상품 테이블
 class Hotel(models.Model):
 
-<<<<<<< HEAD
-    name = models.CharField(max_length=50)
-    
-class Post(models.Model):
-    postname = models.CharField(max_length=50)
-   
-    contents = models.TextField()
-    
-    def __str__(self):
-        return self.postname
-
-# 상품 테이블
-class tb_product(models.Model):
-    
-    #pro_seq = models.CharField(primary_key=True, max_length=200)
-    #mem_seq = models.ForeignKey
-    title = models.CharField(max_length=50)
-    contents = models.TextField()
-    place = models.TextField()
-    adress = models.CharField(max_length=255)
-    #type = 체크박스
-    type = models.CharField(max_length=50, default='')
-    start_dt = models.DateTimeField('date published')
-    end_dt = models.DateTimeField('date published')
-    pro_price = models.IntegerField(default=0)
-    reg_dt = models.DateTimeField(auto_now_add=True)
-    reg_id = models.CharField(max_length=50, default='admin')
-    thumb_file = models.ImageField(null=True, upload_to="thumb/%Y/%m/%d")
-    reserve_yn = models.CharField(max_length = 1, default='N')
-    del_yn = models.CharField(max_length = 1, default='N')
-
-    def __str__(self):
-        return self.title
-=======
     mem_seq = models.ForeignKey("users.User", on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     contents = models.TextField()
@@ -80,4 +46,3 @@ class RoomType(models.Model):
 
     def __str__(self):
         return self.name
->>>>>>> c5d96df0ffcef99a0a37c7a9116c9581c6829eaf
