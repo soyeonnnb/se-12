e403adbc6a30b311d1731d147431ad123c5d5e7b
soyeonnnb <la28s5d@naver.com>
2021-12-09 12:38:33 +0900

    feat: 잘못 pull함

--- bftour/hotels/admin.py
+++ bftour/hotels/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from . import models
+from .models import Post, tb_product
+# Register your models here.
 
+<<<<<<< HEAD
 admin.site.register(models.Hotel)
 admin.site.register(models.RoomType)
+=======
+#Admin이 게시글에 접근 가능하게 함.
+admin.site.register(Post)
+admin.site.register(tb_product)
+>>>>>>> hotel
--- bftour/hotels/views.py
+++ bftour/hotels/views.py
@@ -1,1 +1,25 @@
 from django.shortcuts import render
+<<<<<<< HEAD
+=======
+
+from .models import Post, tb_product
+
+# Create your views here.
+def index(request):
+    hotels = tb_product.objects.all()
+    context = {'hotels': hotels}
+    return render(request, 'hotels/index.html',context)
+
+def blog(request):
+    postlist = Post.objects.all()
+    #모든 Post를 가져와 list에 저장하고 페이지를 열때 list로 가져오게 함
+    return render(request, 'hotels/blog.html', {'postlist':postlist})
+
+#게시글(posting)을 부르는 posting 함수
+def posting(request, pk):
+    # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)를 검색
+    hotels = tb_product.objects.get(pk=pk)
+     # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
+    return render(request, 'hotels/posting.html', {'hotels':hotels})
+
+>>>>>>> hotel
