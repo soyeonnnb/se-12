66c798c7f2e8363e8f21267adc788742935f5e8a
[seinShin] <tlstpdls13@gmail.com>
2021-12-09 23:43:55 +0900

    장소 삭제/ 주소 추가

--- bftour/hotels/forms.py
+++ bftour/hotels/forms.py
@@ -5,13 +5,13 @@ from rooms.models import Room
 class MakeHotel(forms.ModelForm):
    class Meta:
        model = Hotel
-       fields = ['mem_seq','title', 'contents', 'place', 'address',  'start_dt',
+       fields = ['mem_seq','title', 'contents',  'address',  'start_dt',
                  'end_dt', 'pro_price', 'thumb_file' ]
        labels={
            'mem_seq' : '등록자',
            'title' :'상품명',
            'contents' :'상품내용',
-           'place' :'장소',
+           
            'address' :'주소',
            'start_dt' :'상품시작일',
            'end_dt' :'상품종료일',
